588526c238dca197e03ef43b28c31a64516a5664
Mukesh Kumar <mukesh.kumar401514@gmail.com>
2024-08-31 09:36:22 +0530

    added test case for the subtractions

--- test_calculator.py
+++ test_calculator.py
@@ -6,6 +6,7 @@ class TestCalulator(unittest.TestCase):
     def setUp(self):
         self.calulator = Calulator()
 
+    # test cases for the addition
     def test_add_numbers_returns_sum(self):
         result = self.calulator.add(20, 30)
         self.assertEqual(20 + 30, result)
@@ -20,7 +21,7 @@ class TestCalulator(unittest.TestCase):
         self.assertRaises(TypeError, self.calulator.add, '10.02.23.26', 20)
         self.assertRaises(TypeError, self.calulator.add, 20, '10.02.23.26')
 
-    def test_add_string_numbers0_returns(self):
+    def test_add_string_numbers_returns(self):
         result = self.calulator.add("25", "87.2")
         self.assertEqual(25 + 87.2, result)
 
@@ -29,3 +30,28 @@ class TestCalulator(unittest.TestCase):
 
         result = self.calulator.add("25", 87.2)
         self.assertEqual(25 + 87.2, result)
+
+    # test cases for the subtractions
+    def test_subtract_numbers_returns_subtraction(self):
+        result = self.calulator.subtract(40, 30)
+        self.assertEqual(40 - 30, result)
+
+        result = self.calulator.subtract(200.56, 30.56)
+        self.assertEqual(200.56 - 30.56, result)
+
+    def test_subtract_non_numbers_raise_type_error(self):
+        self.assertRaises(TypeError, self.calulator.subtract, 'Hello', 'world')
+        self.assertRaises(TypeError, self.calulator.subtract, 10, 'world')
+        self.assertRaises(TypeError, self.calulator.subtract, 'Hello', 20)
+        self.assertRaises(TypeError, self.calulator.subtract, '10.02.23.26', 20)
+        self.assertRaises(TypeError, self.calulator.subtract, 20, '10.02.23.26')
+
+    def test_subtract_string_numbers_returns(self):
+        result = self.calulator.subtract("250", "87.2")
+        self.assertEqual(250 - 87.2, result)
+
+        result = self.calulator.subtract(25, "87.2")
+        self.assertEqual(25 - 87.2, result)
+
+        result = self.calulator.subtract("25", 87.2)
+        self.assertEqual(25 - 87.2, result)
