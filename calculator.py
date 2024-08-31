ee50a2899f880f2e21b6abea6479f8a02f180e76
Mukesh Kumar <mukesh.kumar401514@gmail.com>
2024-08-31 09:57:43 +0530

    added functionality of the test cases for the subtractions

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 class Calulator:
-    def add(self, x, y):
+    @staticmethod
+    def add(x, y):
         if not isinstance(x, int) and not isinstance(x, float):
             if not x.replace('.', '', 1).isnumeric():
                 raise TypeError
@@ -10,8 +11,17 @@ class Calulator:
             y = float(y)
         return x + y
 
-    def subtract(self, x, y):
-        pass
+    @staticmethod
+    def subtract(x, y):
+        if not isinstance(x, int) and not isinstance(x, float):
+            if not x.replace('.', '', 1).isnumeric():
+                raise TypeError
+            x = float(x)
+        if not isinstance(y, int) and not isinstance(y, float):
+            if not y.replace('.', '', 1).isnumeric():
+                raise TypeError
+            y = float(y)
+        return x - y
 
     def multiply(self, x, y):
         pass
