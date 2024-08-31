09957fb6254f7c094c74b8d8b5239f9e6eaa3604
Mukesh Kumar <mukesh.kumar401514@gmail.com>
2024-08-31 10:13:56 +0530

    added the functionlity of multiplication

--- calculator.py
+++ calculator.py
@@ -23,8 +23,17 @@ class Calulator:
             y = float(y)
         return x - y
 
-    def multiply(self, x, y):
-        pass
+    @staticmethod
+    def multiply(x, y):
+        if not isinstance(x, int) and not isinstance(x, float):
+            if not x.replace('.', '', 1).isnumeric():
+                raise TypeError
+            x = float(x)
+        if not isinstance(y, int) and not isinstance(y, float):
+            if not y.replace('.', '', 1).isnumeric():
+                raise TypeError
+            y = float(y)
+        return x * y
 
     def divide(self, x, y):
         pass
