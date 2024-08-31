ff0e0e00e6e2c83ac8e29338523115a409294010
Mukesh Kumar <mukesh.kumar401514@gmail.com>
2024-08-31 09:26:25 +0530

    added test cases solution for the add functionnailty

--- calculator.py
+++ calculator.py
@@ -1,6 +1,14 @@
 class Calulator:
     def add(self, x, y):
-        return x + y # it is a primitive solution
+        if not isinstance(x, int) and not isinstance(x, float):
+            if not x.replace('.', '', 1).isnumeric():
+                raise TypeError
+            x = float(x)
+        if not isinstance(y, int) and not isinstance(y, float):
+            if not y.replace('.', '', 1).isnumeric():
+                raise TypeError
+            y = float(y)
+        return x + y
 
     def subtract(self, x, y):
         pass
